bb8ceca92da1b23864d727ceb74215c155025483
rck <riccardoventurino0@gmail.com>
2025-05-13 20:45:46 -0300

    Add files via upload

---
+++ aula18/script.js
@@ -0,0 +1,2 @@
+document.write("<h3>Olá, mundo!</h3>");
+document.getElementById("resposta").innerHTML = "O meu nome é " + window.prompt("Qual é o seu nome?");
---
+++ aula18/script2.js
@@ -0,0 +1,5 @@
+function enviaDados()
+{
+    document.getElementById("Inputusuario").innerHTML = "Nome enviado foi : " + window.prompt("Digite seu nome: ")
+};
+window.alert("Nome digitado foi : " + document.getElementById("getele").value)
